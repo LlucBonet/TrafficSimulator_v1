5c96548ff9aaec5183d64092635e975f9f3e0b7e
LlucBonet <60605423+LlucBonet@users.noreply.github.com>
2020-03-03 10:29:23 +0100

    Add files via upload

---
+++ RoundRobinStrategyBuilder.java
@@ -0,0 +1,20 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+import simulator.model.LightSwitchStrategy;
+
+public class RoundRobinStrategyBuilder extends Builder<LightSwitchStrategy> {
+
+	RoundRobinStrategyBuilder(String type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected LightSwitchStrategy createTheInstance(JSONObject data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
