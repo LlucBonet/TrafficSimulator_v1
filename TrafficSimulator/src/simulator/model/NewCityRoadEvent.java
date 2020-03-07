20f944424177a1b7e95e5d911c6cb04c340ebbfa
lluc <maria@LAPTOP-1IJ2802G>
2020-03-08 00:35:32 +0100

    cambio algunos nombres de las variables pq asi me aclaro mejor

--- TrafficSimulator/src/simulator/model/NewCityRoadEvent.java
+++ TrafficSimulator/src/simulator/model/NewCityRoadEvent.java
@@ -5,28 +5,26 @@ public class NewCityRoadEvent extends NewRoadEvent {
 	protected String _id;
 	protected String _srcJ;
 	protected String _destJ;
-	protected int _lngth;
-	protected int _co2L;
-	protected int _maxS;
-	protected Weather _wther;
+	protected int _length;
+	protected int _co2Limit;
+	protected int _maxSpeed;
+	protected Weather _weather;
 	
 
 	public NewCityRoadEvent(int time, String id, String srcJun, String destJunc, int length, int co2Limit, int maxSpeed, Weather weather) {
 		super(time);
-		// TODO Auto-generated constructor stub
 		_id = id;
 		_srcJ = srcJun;
 		_destJ = destJunc;
-		_lngth = length;
-		_co2L = co2Limit;
-		_maxS = maxSpeed;
-		_wther = weather;
+		_length = length;
+		_co2Limit = co2Limit;
+		_maxSpeed = maxSpeed;
+		_weather = weather;
 	}
 
 	@Override
 	Road createRoadObject(RoadMap map) throws Exception {
-		// TODO Auto-generated method stub
-		return new CityRoad(_id, map.getJunction(_srcJ), map.getJunction(_destJ), _lngth, _co2L, _maxS, _wther);
+		return new CityRoad(_id, map.getJunction(_srcJ), map.getJunction(_destJ), _length, _co2Limit, _maxSpeed, _weather);
 	}
 	
 	
